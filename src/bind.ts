25c8977451b1a8d9904b52a09050cd44c4f93cbb
Ayub-Begimkulov <begimkulov963@gmail.com>
2021-05-27 09:06:34 +0300

    event type is infered from target

--- src/bind.ts
+++ src/bind.ts
@@ -1,6 +1,10 @@
 import { UnbindFn, Binding } from './types';
 
-export function bind(target: EventTarget, { type, listener, options }: Binding): UnbindFn {
+export function bind<Target extends EventTarget, Type extends string>(
+  target: Target,
+  { type, listener, options }: Binding<Target, Type>,
+): UnbindFn;
+export function bind(target: EventTarget, { type, listener, options }: Binding) {
   target.addEventListener(type, listener, options);
 
   return function unbind() {
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,31 @@
 export type UnbindFn = () => void;
 
-export type Binding = {
-  type: string;
-  listener: EventListenerOrEventListenerObject;
+type AnyFunction = (...args: any[]) => any;
+
+type GetEventType<Target extends EventTarget, Type extends string> =
+  `on${Type}` extends keyof Target
+    ? GetEventTypeFromListener<
+        // remove types that aren't assignable to `AnyFunction`
+        // so that we don't end up with union like `MouseEvent | Event`
+        Extract<Target[`on${Type}`], AnyFunction>
+      >
+    : Event;
+
+type GetEventTypeFromListener<T extends AnyFunction> = T extends (this: any, event: infer U) => any
+  ? U extends Event
+    ? U
+    : Event
+  : Event;
+
+export type Binding<Target extends EventTarget = EventTarget, Type extends string = string> = {
+  type: Type;
+  listener: Listener<GetEventType<Target, Type>>;
   options?: boolean | AddEventListenerOptions;
 };
+
+type Listener<Ev extends Event> =
+  | { handleEvent(e: Ev): void }
+  // using bivariance hack here so if the user
+  // wants to narrow event type by hand TS
+  // won't give him an error
+  | { bivarianceHack(e: Ev): void }['bivarianceHack'];
