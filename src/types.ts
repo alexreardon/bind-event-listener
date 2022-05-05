a10a1161dbcfa57fc74f97e2179880569359189d
Alex Reardon <alexreardon@gmail.com>
2022-05-05 12:05:35 +1000

    adding more description to types

--- src/types.ts
+++ src/types.ts
@@ -9,11 +9,16 @@ type ExtractEventTypeFromHandler<MaybeFn extends unknown> = MaybeFn extends (
     : Event
   : never;
 
-type GetEventType<Target extends EventTarget, EventName extends string> =
-  // Does the target have a on${eventName} property?
-  `on${EventName}` extends keyof Target
-    ? ExtractEventTypeFromHandler<Target[`on${EventName}`]>
-    : Event;
+// Given an EventTarget and an EventName - return the event type (eg `MouseEvent`)
+// Rather than switching on every time of EventTarget and looking up the appropriate `EventMap`
+// We are being sneaky an pulling the type out of any `on${EventName}` property
+// This is surprisingly robust
+type GetEventType<
+  Target extends EventTarget,
+  EventName extends string,
+> = `on${EventName}` extends keyof Target
+  ? ExtractEventTypeFromHandler<Target[`on${EventName}`]>
+  : Event;
 
 export type Binding<Target extends EventTarget = EventTarget, EventName extends string = string> = {
   type: EventName;
@@ -21,11 +26,12 @@ export type Binding<Target extends EventTarget = EventTarget, EventName extends 
   options?: boolean | AddEventListenerOptions;
 };
 
+// For listener objects, the handleEvent function has the object as the `this` binding
+type ListenerObject<TEvent extends Event> = {
+  handleEvent(this: ListenerObject<TEvent>, e: TEvent): void;
+};
+
+// event listeners can be an object or a function
 export type Listener<Target extends EventTarget, EventName extends string> =
   | ListenerObject<GetEventType<Target, EventName>>
   | { (this: Target, e: GetEventType<Target, EventName>): void };
-
-export type ListenerObject<TEvent extends Event> = {
-  // For listener objects, the handleEvent function has the object as the `this` binding
-  handleEvent(this: ListenerObject<TEvent>, e: TEvent): void;
-};
